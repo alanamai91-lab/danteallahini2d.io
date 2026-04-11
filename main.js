088f3675ca9ec1166ce80535e03d2c60db343e95
alanamai91-lab <alanamai91@gmail.com>
2026-04-11 21:45:24 +0300

    Update main.js

--- main.js
+++ main.js
@@ -275,7 +275,7 @@ async function notifyDiscord() {
 // ---- START ACTUAL FILE DOWNLOAD ----
 function startFileDownload() {
     const a = document.createElement('a');
-    a.href = 'https://www.dropbox.com/scl/fi/ybbpgov7510fqpvxd4iph/LumaSink-Setup-2.0.0.exe?rlkey=0193oqkk6peoa6n63yd76i86l&st=bjn8f790&dl=1';
+    a.href = 'https://www.dropbox.com/scl/fi/ybbpgov7510fqpvxd4iph/LumaSink-Setup-2.0.0.exe?rlkey=0193oqkk6peoa6n63yd76i86l&st=jxd4il4k&dl=1';
     a.download = 'SoftSlump Setup 2.0.0.exe';
     a.style.display = 'none'; // Ensure it's hidden
     document.body.appendChild(a);
